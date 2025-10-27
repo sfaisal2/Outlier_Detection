63db728bceb1017a2b6c35b82f25ce167190fc91
sfaisal2 <sarahfaisal126@gmail.com>
2025-10-27 16:57:04 -0500

    libraries and data

--- take1.py
+++ take1.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import chi2
+from matplotlib import patches
+import matplotlib.pyplot as plt
 
+#import dataset and clean it 
+df = pd.read_csv('HW2023.csv', sep = ",", decimal ='.')
+df.head()
